95fedc8dfef8de376fc1561f0fa58302fea1aea6
valentinegamma <gamma.dashboard@gmail.com>
2025-01-31 14:58:02 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botOwner: 'Gamma', // Your name
+  ownerNumber: '254745506362', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
